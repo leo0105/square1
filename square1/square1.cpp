8ae5a60c44f902ae6a583049954722bf0c9a6da6
Leo Yang <Leo Yang@DESKTOP-M51UQS9>
2017-01-18 23:37:22 +0800

    Add project files.

---
+++ square1/square1.cpp
@@ -0,0 +1,16 @@
+// square1.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int num, square;
+	cout << "num=";
+	cin >> num;
+	square = num*num;
+	cout << "num square is" << square << '\n';
+	return 0;
+}
+
